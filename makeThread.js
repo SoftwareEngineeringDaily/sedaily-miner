a2c7b0254e8ff9eb956292e30ebf3b7b2538ea26
jason <bjason@gmail.com>
2018-04-19 12:18:42 -0700

    Working insert forum thread!

--- makeThread.js
+++ makeThread.js
@@ -1,18 +1,21 @@
 require('dotenv').config()
-const db = require('monk')(process.env.MONGO_DB)
+const monk = require('monk');
+const db = require('monk')(process.env.MONGO_DB);
 const rp = require('request-promise');
 const threads = db.get('forumthreads')
 const Bluebird = require('bluebird');
 const moment = require('moment');
 
+
 threads.insert({
-  title: 'Bot: Testing',
+  title: 'Bot: Testing!',
   content: 'Some post ',
-  author: '5ad6865b51d34d1c5f049bd5',
+  author: monk.id('5ad6865b51d34d1c5f049bd5'),
   score: 0 ,
+  deleted: false,
   commentsCount: 0,
-  dateCreated: Date.now(),
-  dateLastAcitiy:  Date.now()
+  dateCreated: moment(Date.now()).toDate() ,
+  dateLastAcitiy:   moment(Date.now()).toDate() ,
 }).then( (thread) => {
   console.log('Thread created---');
   console.log(thread);
