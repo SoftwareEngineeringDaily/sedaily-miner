4a7fd0f251759ca5d4a6a2ed5ac226378039b067
Jason Bautista <jasoncb@cs.stanford.edu>
2018-05-29 12:36:02 -0700

    Fix getMp3 script

--- getMp3.js
+++ getMp3.js
@@ -4,38 +4,37 @@ const posts = db.get('posts')
 const getUrls = require('get-urls');
 const Bluebird = require('bluebird');
 const rp = require('request-promise');
+const request = require("request");
 
 let promises = [];
 posts.find( {mp3: {$exists: false}})
   .each((post) => {
-    let urls = getUrls(post.content.rendered);
-    let values = urls.values();
-    let mp3 = '';
-    let mainImage = '';
 
-    for (let url of values) {
-      let extension = url.substr(url.length - 4);
+    request({
+      uri: post.link,
+    }, function(error, response, body) {
+      const urls = getUrls(body);
 
-      if (extension === '.mp3') {
-        mp3 = url;
-        break;
+      let values = urls.values();
+      var mp3 = null;
+      for (let url of values) {
+        let extension = url.substr(url.length - 4);
+
+        if (extension === '.mp3' && url.indexOf('libsyn.com/sedaily') >= 0) {
+          mp3 = url;
+          break;
+        }
       }
-    }
 
-    if (mp3) {
-      let promise = posts.update({id: post.id}, {
-        $set: {
-          mp3,
-        },
-      });
-      promises.push(promise);
-    }
-  })
-  .then(() => {
-    return Bluebird.all(promises);
-  })
-  .then(() => {
-    console.log("done");
-    process.exit();
+      if (mp3) {
+         posts.update({id: post.id}, {
+          $set: {
+            mp3,
+          },
+        })
+        .then((result) => { console.log('sucess updating', mp3); })
+        .catch((error) => { console.log(error); })
+      }
+    });
   })
   .catch((error) => { console.log(error); })
