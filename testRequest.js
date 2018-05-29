b655d236617b7b59e0056c8dc5c37e85be776793
Jason Bautista <jasoncb@cs.stanford.edu>
2018-05-29 12:33:30 -0700

    Match getMp3

--- testRequest.js
+++ testRequest.js
@@ -1,4 +1,4 @@
-var request = require("request");
+const request = require("request");
 const getUrls = require('get-urls');
 
 request({
@@ -6,8 +6,9 @@ request({
 }, function(error, response, body) {
   const urls = getUrls(body);
 
+  let values = urls.values();
   var mp3 = null;
-  for (let url of urls) {
+  for (let url of values) {
     let extension = url.substr(url.length - 4);
 
     if (extension === '.mp3' && url.indexOf('libsyn.com/sedaily') >= 0) {
