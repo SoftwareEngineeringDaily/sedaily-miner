20c8273320390f5fa0b2b46af463e12f638d0327
Jason Bautista <jasoncb@cs.stanford.edu>
2018-05-29 15:09:54 -0700

    Last replacement of post id

--- getMp3.js
+++ getMp3.js
@@ -11,6 +11,7 @@ let promises = [];
 let fakePlayer = `<!--powerpress_player--><div class="powerpress_player" id="powerpress_player_9728"><!--[if lt IE 9]><script>document.createElement('audio');</script><![endif]-->↵<audio class="wp-audio-shortcode" id="audio-4917-1" preload="none" style="width: 100%;" controls="controls"><source type="audio/mpeg" src="http://traffic.libsyn.com/sedaily/2018_05_21_VoiceRecognitionAnalysis.mp3?_=1" /><a href="http://traffic.libsyn.com/sedaily/2018_05_21_VoiceRecognitionAnalysis.mp3">http://traffic.libsyn.com/sedaily/2018_05_21_VoiceRecognitionAnalysis.mp3</a></audio></div><p class="powerpress_links powerpress_links_mp3">Podcast: <a href="http://traffic.libsyn.com/sedaily/2018_05_21_VoiceRecognitionAnalysis.mp3" class="powerpress_link_pinw" target="_blank" title="Play in new window" onclick="return powerpress_pinw('https://softwareengineeringdaily.com/?powerpress_pinw=4917-podcast');" rel="nofollow">Play in new window</a> | <a href="http://traffic.libsyn.com/sedaily/2018_05_21_VoiceRecognitionAnalysis.mp3" class="powerpress_link_d" title="Download" rel="nofollow" download="2018_05_21_VoiceRecognitionAnalysis.mp3">Download</a></p>`
 let placeHolder = `http://traffic.libsyn.com/sedaily/2018_05_21_VoiceRecognitionAnalysis.mp3`;
 let placeHolder2 = `2018_05_21_VoiceRecognitionAnalysis.mp3`;
+let placeHolder3 = `4917`;
 
 
 posts.find( {mp3: {$exists: false}})
@@ -34,7 +35,7 @@ posts.find( {mp3: {$exists: false}})
 
       if (mp3) {
         // HACK so current build of iOS doesn't break:
-        let newPreContent = fakePlayer.split(placeHolder).join(mp3).split(placeHolder2).join('');
+        let newPreContent = fakePlayer.split(placeHolder).join(mp3).split(placeHolder2).join('').split(placeHolder3).join(post.id);
 
         let content = { protected: false, rendered: newPreContent + post.content.rendered};
 
