5a2a97a55ab7a3ca3b9ce3866380871b9d175085
kamilsiwek <kontokamilsiwek@gmail.com>
2019-03-18 12:01:52 +0100

    Add generateTopics/js file and removeTopicsFromPosts.js file

--- generateTopics.js
+++ generateTopics.js
@@ -48,47 +48,49 @@ function createTopics() {
     await addTopicsToDB()
   })
   .then(async () => {
-    await addTopicsToPosts();
+    await addTopicsToPosts()
+    .then(() => {
+      console.log("SUCCESS");
+      process.exit();
+    })
   });
 }
 
-async function addTopicsToDB() {
-  for (let k = 0; k<newTopics.length; k++) {
-    await topics.insert(newTopics[k])
-  }
-}
 
 async function addTopicsToPosts() {
   let promises2 = []
-  posts.find({ filterTags: { $exists: true }, topicsGenerated: { $exists: false }}).each(async function(post) {
+  async function asyncTopicToPosts(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+  }
+
+  const postsWithTags = await posts.find({ filterTags: { $exists: true }, topicsGenerated: { $exists: false }})
+
+  await asyncTopicToPosts(postsWithTags, async (post) => {
 
     if (post.filterTags.length != 0) {
-      let k = 0;
       for (k = 0; k<post.filterTags.length; k++) {
-        // post.filterTags.map(async tag => {
-          const tag = post.filterTags[k]
+        const tag = post.filterTags[k]
 
-          const tagName = tag.name;
+        const tagName = tag.name;
 
-          let existingTopic = await topics.findOne({ name: tagName })
-          //.then(async existingTopic => {
+        let existingTopic = await topics.findOne({ name: tagName })
+          await posts.update({ _id: post._id },
+            {
+              $push: {
+                topics: existingTopic._id.toString()
+              },
+              $set: { topicsGenerated: true }
+            }, (error) => {
+              if (error) return;
+            });
 
-            // update topic if is contained in existingTopic:
-            await posts.update({ _id: post._id },
-              {
-                $push: {
-                  topics: existingTopic._id.toString()
-                },
-                $set: { topicsGenerated: true }
-              }, (error) => {
-                if (error) return;
-              });
-
-              await topics.update({ _id: existingTopic._id }, {
-                $inc: { postCount: 1 }
-              }, (err) => {
-                if (err) return;
-              });
+            await topics.update({ _id: existingTopic._id }, {
+              $inc: { postCount: 1 }
+            }, (err) => {
+              if (err) return;
+            });
           }
         }
       })
@@ -109,7 +111,10 @@ function generateSlug(string) {
     .replace(/-+$/, "");
 }
 
-
-
-
 createTopics()
+
+async function addTopicsToDB() {
+  for (let k = 0; k<newTopics.length; k++) {
+    await topics.insert(newTopics[k])
+  }
+}
