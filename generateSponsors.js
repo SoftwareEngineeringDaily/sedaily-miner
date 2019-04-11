65929ea76610ba9babce0d117f41402df995b597
kamilsiwek <kontokamilsiwek@gmail.com>
2019-04-11 19:43:48 +0200

    Update all posts

--- generateSponsors.js
+++ generateSponsors.js
@@ -1,67 +1,102 @@
 require('dotenv').config()
-const HTML = require('html-parse-stringify');
-const _ =require('lodash');
 const db = require('monk')(process.env.MONGO_DB)
 const posts = db.get('posts')
-const Bluebird = require('bluebird');
-const getUrls = require('get-urls');
 const rp = require("request-promise");
 const moment = require("moment");
 const async = require('async');
 const request = require("request");
 
 const SET_NULL_AFTER_DAYS = 30; // The number of days after script sets transcript URL to null if not set yet
+let counter = 0
+const CONCURRENCY = 5;
 
-let promises = [];
-let postsCount = 0;
-posts.find({sponsorsContent: {$exists: false}}, { stream: false })
-  .each((post) => {
-    postsCount++
+var q = async.queue(function(post, callback) {
+  date_now = moment.utc();
+  request({
+    uri: post.link,
+  }, function(error, response, body) {
+    try {
+      let sponsorsContent = null
+      sponsorsContent = body.split('<h3>Sponsors</h3>')
 
-    date_now = moment.utc();
+      if (sponsorsContent) {
+        if (sponsorsContent.length == 2) {
 
-    let requestPromise = rp(post.link)
-    .then(async function(body) {
-      let sponsorsContent = body.split('<h3>Sponsors</h3>')
+          sponsors = sponsorsContent[1].trim();
+          sponsorsNoWhiteSpaces = sponsors.replace(/\>\s+\</g,'><')
+          let sponsorsCut = sponsorsNoWhiteSpaces.split('</div></div><div class="col-xs-12 col-md-6 col-lg-3">')
+          sponsorsContent = sponsorsCut[0];
 
-      if (sponsorsContent.length == 2) {
-        sponsors = sponsorsContent[1].trim();
-        sponsorsNoWhiteSpaces = sponsors.replace(/\>\s+\</g,'><')
-        let sponsorsCut = sponsorsNoWhiteSpaces.split('</div></div><div class="col-xs-12 col-md-6 col-lg-3">')
-        sponsorsContent = sponsorsCut[0];
-
-        await posts.update({id: post.id}, {
-          $set: {
-            "sponsorsContent": sponsorsContent
-          },
-        })
-        console.log("Add sponsors to: ", post.title["rendered"]);
-      } else {
-        time_diff = date_now.diff(moment(post.date), 'days')
-        if (time_diff > SET_NULL_AFTER_DAYS) {
-          await posts.update({id: post.id}, {
+          posts.update({id: post.id}, {
             $set: {
-              "sponsorsContent": null
+              "sponsorsContent": sponsorsContent
             },
           })
+          .then((result) => {
+            console.log('success updating', post.title["rendered"]);
+            counter ++
+            callback();
+          })
+          .catch((error) => {
+            callback(error);
+          })
+          // console.log("Add sponsors to: ", post.title["rendered"]);
+        } else {
+          time_diff = date_now.diff(moment(post.date), 'days')
+          if (time_diff > SET_NULL_AFTER_DAYS) {
+            posts.update({id: post.id}, {
+              $set: {
+                "sponsorsContent": null
+              },
+            })
+            .then((result) => {
+              counter ++
+              console.log('Update null to: ', post.title["rendered"]);
+              callback();
+            })
+            .catch((error) => {
+              callback(error);
+            })
+          } else {
+            console.log('No sponsors for:', post.title["rendered"])
+            callback();
+          }
         }
+      } else {
+        counter ++
+        console.log('No sponsors for:', post.title["rendered"])
+        callback();
       }
-    })
-    .catch((error) => {
-      if (error.statusCode !== 429 && error.statusCode !== 504) {
-        console.log('Error', error)
-      }
-    })
 
-    promises.push(requestPromise);
-    console.log(postsCount);
-  })
-  .then(() => {
-    console.log('BLUEBIRD')
-    return Bluebird.all(promises);
-  })
-  .then(() => {
-    console.log("done");
+    } catch (e) {
+        console.log(e);
+        callback();
+    }
+
+  });
+}, CONCURRENCY);
+
+q.drain = function() {
+  console.log('all items have been processed');
+  db.close();
+};
+let progress = 0
+posts.count( {sponsorsContent: {$exists: false}})
+.then((c) => {
+  if (c > 0) {
+    posts.find( {sponsorsContent: {$exists: false}})
+      .each((post) => {
+        q.push(post, function (err) {
+          if (err) {
+            console.log(err);
+          } else {
+            progress = Math.round((counter/c)*100)
+            console.log("PROGRESS: " + progress + "%");
+          }
+        });
+      })
+  } else {
+    console.log("All posts updated.");
     process.exit();
-  })
-  .catch((error) => { console.log(error); })
+  }
+})
